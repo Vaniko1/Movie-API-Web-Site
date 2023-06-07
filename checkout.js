f9e7c43c624021090ef4b325d61e5b7e17e9e8a0
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2023-06-07 20:56:34 +0400

    changes made in checkout javascript file

--- checkout.js
+++ checkout.js
@@ -3,8 +3,6 @@ const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
 const main = document.getElementById("main");
 
-const similar = document.getElementById("similar");
-
 const movie = localStorage.getItem("movie");
 console.log(JSON.parse(movie));
 const movieData = JSON.parse(movie);
@@ -28,52 +26,6 @@ movieDesc.innerHTML = `
     </div>
 `;
 main.appendChild(movieDesc);
-
-function showMovies(movies) {
-  similar.innerHTML = " ";
-
-  const smallMovies = (movies = movies
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 3));
-  smallMovies.forEach((movie) => {
-    const { title, overview, original_language, vote_average, poster_path } =
-      movie;
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("col-4");
-    movieEl.innerHTML = `
-                <div class="p-4">
-                <div class="movies">
-                  <img src="${IMG_PATH + poster_path}" >
-                  <div class="movie_content_box">
-                    <h3>${title}</h3>
-                    <p>${overview}</p>
-                    <p>${original_language}</p>
-                    </div>
-                    <span>
-                      <p class="${getClassByVote(
-                        vote_average
-                      )}">${vote_average}</p>
-                    </span>
-                    </div>
-                </div>
-            `;
-    similar.appendChild(movieEl);
-    movieEl.addEventListener("click", () => {
-      localStorage.setItem("movie", JSON.stringify(movie));
-      window.location = "movie.html";
-    });
-  });
-}
-
-function getClassByVote(vote) {
-  if (vote >= 7) {
-    return "green";
-  } else if (vote >= 5) {
-    return "yellow";
-  } else {
-    return "red";
-  }
-}
 
 const bookedPlace = localStorage.getItem("seat");
 
